8ed194c508334281e7251a1f6520cff551acb9f4
asdasdasdadsa <92573461+asdasdasdadsa@users.noreply.github.com>
2021-12-07 20:18:32 +0300

    Прямоугольник во все стороны

--- src/main/kotlin/ru/smak/ui/painting/SelectablePanel.kt
+++ src/main/kotlin/ru/smak/ui/painting/SelectablePanel.kt
@@ -7,6 +7,8 @@ import java.awt.Rectangle
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import java.awt.event.MouseMotionAdapter
+import kotlin.math.abs
+import kotlin.math.min
 
 class SelectablePanel(vararg painters: Painter) : GraphicsPanel(*painters){
 
@@ -38,7 +40,7 @@ class SelectablePanel(vararg painters: Painter) : GraphicsPanel(*painters){
             override fun mouseReleased(e: MouseEvent?) {
                 pt1?.let { p1 ->
                     pt2?.let { p2->
-                        val r: Rectangle = Rectangle(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y)
+                        val r = Rectangle(min(p1.x,p2.x),min(p1.y,p2.y), abs(p2.x - p1.x),abs(p2.y-p1.y))
                         selectListener.forEach { it(r) }
                     }
                 }
@@ -54,11 +56,11 @@ class SelectablePanel(vararg painters: Painter) : GraphicsPanel(*painters){
                     setXORMode(Color.WHITE)
                     pt1?.let { pt ->
                         pt2?.let{ pt2 ->
-                            drawRect(pt.x, pt.y, pt2.x - pt.x, pt2.y - pt.y)
+                            drawRect(min(pt.x,pt2.x),min(pt.y,pt2.y), abs(pt2.x - pt.x),abs(pt2.y-pt.y))
                         }
                         pt2 = e?.point
                         e?.let { e ->
-                            drawRect(pt.x, pt.y, e.x - pt.x, e.y - pt.y)
+                            drawRect(min(pt.x,e.x), min(pt.y,e.y), abs(e.x - pt.x), abs(e.y-pt.y))
                         }
                     }
                     setPaintMode()
