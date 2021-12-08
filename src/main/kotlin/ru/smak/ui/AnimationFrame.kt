49f06a5512e24b5b643d99eb9ed13f9f47a39f2c
Максим Афанасьев <MDAfanasev@stud.kpfu.ru>
2021-12-08 14:55:10 +0300

    Добавлен класс AnimationFrame; Настроено открытие окна с экскурсией по клику на кнопке "Открыть"

---
+++ src/main/kotlin/ru/smak/ui/AnimationFrame.kt
@@ -0,0 +1,74 @@
+package ru.smak.ui
+
+import ru.smak.ui.painting.SelectablePanel
+import java.awt.Color
+import java.awt.Dimension
+import javax.swing.*
+
+class AnimationFrame(private val selectablePanel: SelectablePanel) : JFrame() {
+    val ctrlPanel : JPanel
+    val animLabel : JLabel
+    val frameScroll : JScrollPane
+    val frameScrollPanel : JPanel
+    init {
+        defaultCloseOperation = EXIT_ON_CLOSE
+        title = "Экскурсия"
+        minimumSize = Dimension(1000, 500)
+        animLabel = JLabel().apply {
+            text = "Создание анимации"
+            font = getFont().deriveFont(16.0f)
+        }
+
+        frameScrollPanel = JPanel().apply {
+            background = Color.GREEN
+        }
+        frameScroll = JScrollPane(frameScrollPanel).apply {
+
+        }
+        ctrlPanel = JPanel().apply {
+            background = Color.WHITE
+        }
+        layout = GroupLayout(contentPane).apply {
+            setHorizontalGroup(
+                createSequentialGroup()
+                    .addGap(4)
+                    .addComponent(selectablePanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+                    .addComponent(ctrlPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+                    .addGap(4)
+            )
+            setVerticalGroup(
+                createSequentialGroup()
+                    .addGap(4)
+                    .addGroup(createParallelGroup()
+                        .addComponent(selectablePanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+                        .addComponent(ctrlPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE)
+                    )
+                    .addGap(4)
+            )
+        }
+
+        ctrlPanel.layout = GroupLayout(ctrlPanel).apply {
+            setHorizontalGroup(
+                createSequentialGroup()
+                    .addGap(50)
+                    .addGroup(
+                        createParallelGroup(GroupLayout.Alignment.CENTER)
+                            .addComponent(animLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+                            .addGap(50)
+                            .addComponent(frameScrollPanel, 250, 250, 250)
+                    )
+                    .addGap(50)
+            )
+            setVerticalGroup(
+                createSequentialGroup()
+                    .addGap(15)
+                    .addComponent(animLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+                    .addGap(50)
+                    .addComponent(frameScrollPanel, 300, GroupLayout.PREFERRED_SIZE , GroupLayout.PREFERRED_SIZE)
+                    .addGap(30, 30 , Int.MAX_VALUE)
+            )
+        }
+    }
+
+
+}
--- src/main/kotlin/ru/smak/ui/MainFrame.kt
+++ src/main/kotlin/ru/smak/ui/MainFrame.kt
@@ -7,17 +7,25 @@ import ru.smak.ui.painting.fractals.FractalPainter
 import ru.smak.ui.painting.fractals.colorizers
 import java.awt.Color
 import java.awt.Dimension
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
 import javax.swing.GroupLayout
+import javax.swing.JButton
 import javax.swing.JFrame
 import kotlin.random.Random
 
 class MainFrame : JFrame() {
-
+    val animFrame : AnimationFrame
+    val openAnimFrame : JButton
     val fractalPanel: SelectablePanel
 
     init{
         defaultCloseOperation = EXIT_ON_CLOSE
         minimumSize = Dimension(600, 400)
+
+        openAnimFrame = JButton().apply {
+            text = "Открыть экскурсию"
+        }
         val painter = FractalPainter(
             Mandelbrot(),
             CartesianPlane(-2.0, 1.0, -1.0, 1.0),
@@ -37,18 +45,26 @@ class MainFrame : JFrame() {
                 repaint()
             }
         }
+        animFrame = AnimationFrame(fractalPanel)
+        openAnimFrame.addMouseListener( object : MouseAdapter() {
+            override fun mouseClicked(e: MouseEvent?) {
+                animFrame.isVisible = true
+            }
+        })
         
         layout = GroupLayout(contentPane).apply {
             setHorizontalGroup(
                 createSequentialGroup()
                     .addGap(4)
                     .addComponent(fractalPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+                    .addComponent(openAnimFrame, 100, 100, 100)
                     .addGap(4)
             )
             setVerticalGroup(
                 createSequentialGroup()
                     .addGap(4)
                     .addComponent(fractalPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+                    .addComponent(openAnimFrame, 100, 100, 100)
                     .addGap(4)
             )
         }
