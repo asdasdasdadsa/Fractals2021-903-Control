81a06245f3831924712e80543faf7b59088911d1
Максим Афанасьев <MDAfanasev@stud.kpfu.ru>
2021-12-11 23:20:28 +0300

    Добавлен класс ImagePainter для создания и масштабирования изображения с основной панели и его последующим выводом на панельки с ключевыми кадрами. Также добавлен класс KeyFramePanel для работы непосредственно с панелями ключевых кадров

--- src/main/kotlin/ru/smak/ui/AnimationFrame.kt
+++ src/main/kotlin/ru/smak/ui/AnimationFrame.kt
@@ -1,32 +1,55 @@
 package ru.smak.ui
 
+import ru.smak.ui.painting.ImagePainter
+import ru.smak.ui.painting.KeyFramePanel
 import ru.smak.ui.painting.SelectablePanel
 import java.awt.Color
 import java.awt.Dimension
+import java.awt.Image
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+import java.awt.image.BufferedImage
+import java.io.BufferedReader
 import javax.swing.*
 
 class AnimationFrame(private val selectablePanel: SelectablePanel) : JFrame() {
     val ctrlPanel : JPanel
     val animLabel : JLabel
-    val frameScroll : JScrollPane
-    val frameScrollPanel : JPanel
+    //val frameScroll : JScrollPane
+    val keyFramesPanel : JPanel
+    val addKeyFrame : JButton
     init {
         defaultCloseOperation = EXIT_ON_CLOSE
         title = "Экскурсия"
         minimumSize = Dimension(1000, 500)
+        val keyFrames = arrayListOf<JPanel>()
+
         animLabel = JLabel().apply {
             text = "Создание анимации"
             font = getFont().deriveFont(16.0f)
         }
-
-        frameScrollPanel = JPanel().apply {
-            background = Color.GREEN
-        }
-        frameScroll = JScrollPane(frameScrollPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER).apply {
+        keyFramesPanel = JPanel().apply {
+            background = Color.GRAY
         }
 
+      //   frameScroll = JScrollPane(keyFramesPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER).apply {
+      //  }
+        addKeyFrame = JButton().apply {
+            text = "Добавить ключевой кадр"
+        }
+
+        addKeyFrame.addMouseListener(object : MouseAdapter() {
+            override fun mouseClicked(e: MouseEvent?) {
+                val img = BufferedImage(selectablePanel.width, selectablePanel.height, BufferedImage.TYPE_INT_RGB)
+                val imgGr = img.createGraphics()
+                selectablePanel.paint(imgGr)
+                val keyFrame = KeyFramePanel(0, ImagePainter(img, Dimension(250, 150)))
+                keyFramesPanel.add(keyFrame)
+            }
+        })
         ctrlPanel = JPanel().apply {
             background = Color.WHITE
+            border = BorderFactory.createLineBorder(Color.BLACK)
         }
         layout = GroupLayout(contentPane).apply {
             setHorizontalGroup(
@@ -46,7 +69,6 @@ class AnimationFrame(private val selectablePanel: SelectablePanel) : JFrame() {
                     .addGap(4)
             )
         }
-
         ctrlPanel.layout = GroupLayout(ctrlPanel).apply {
             setHorizontalGroup(
                 createSequentialGroup()
@@ -55,7 +77,9 @@ class AnimationFrame(private val selectablePanel: SelectablePanel) : JFrame() {
                         createParallelGroup(GroupLayout.Alignment.CENTER)
                             .addComponent(animLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
                             .addGap(30)
-                            .addComponent(frameScroll, 250, 250, 250)
+                            .addComponent(keyFramesPanel, 250, 250, 250)
+                            .addGap(10)
+                            .addComponent(addKeyFrame, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
                     )
                     .addGap(50)
             )
@@ -64,10 +88,13 @@ class AnimationFrame(private val selectablePanel: SelectablePanel) : JFrame() {
                     .addGap(15)
                     .addComponent(animLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
                     .addGap(30)
-                    .addComponent(frameScroll, 300, GroupLayout.PREFERRED_SIZE , GroupLayout.PREFERRED_SIZE)
+                    .addComponent(keyFramesPanel, 300, GroupLayout.PREFERRED_SIZE , GroupLayout.PREFERRED_SIZE)
+                    .addGap(10)
+                    .addComponent(addKeyFrame, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
                     .addGap(30, 30 , Int.MAX_VALUE)
             )
         }
+
     }
 
 
