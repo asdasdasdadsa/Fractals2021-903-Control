43109afff8398a3280ba07b1679e48554c95e04f
annnn <AnnnaSukhanova>
2021-12-14 16:24:16 +0400

    добавлены комментарии

--- src/main/kotlin/ru/smak/ui/painting/SaveImg.kt
+++ src/main/kotlin/ru/smak/ui/painting/SaveImg.kt
@@ -10,32 +10,29 @@ import javax.swing.JLabel
 
 
 class SaveImg (private val plane: CartesianPlane){
-    private val  saveFileChooser: JFileChooser = JFileChooser()
+    private val saveFileChooser: JFileChooser = JFileChooser()
     private val newBI: BufferedImage = BufferedImage()
     private val messageLabel: JLabel=JLabel()
 
 
     init {
-        saveFileChooser.currentDirectory = File("C:\\Desktop")
-        saveFileChooser.fileFilter = FileNameExtensionFilter("PNG images", "png")
+        saveFileChooser.currentDirectory = File("C:\\Desktop") //Метод определения текущей директории
+        saveFileChooser.fileFilter = FileNameExtensionFilter("PNG images", "png") //Метод установки файлового фильтра
     }
 
 
     fun saveImage (fractalPanel: SelectablePanel){
-        var returnValue = saveFileChooser.showSaveDialog()
-        if(returnValue == JFileChooser.APPROVE_OPTION){
+        var returnValue = saveFileChooser.showSaveDialog(saveFileChooser)//Функция открытия диалогового окна «Сохранить файл»
+        if(returnValue == JFileChooser.APPROVE_OPTION){ //выбор файла в диалоговом окне прошел успешно; выбранный файл можно получить методом getFile()
             try {
                 ImageIO.write(newBI,"png",saveFileChooser.selectedFile)
                 messageLabel.text = "Изображение успешно загружено"
-            } catch (IOException e ){
+            } catch (IOException ioe ){
                 messageLabel.text = "Не удалось загрузить изображение"
             }
         }else {
             messageLabel.text = "Ошибка"
         }
-
-
-
 
     }
 }
