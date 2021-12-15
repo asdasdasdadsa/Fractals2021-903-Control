f43906335aaadda53985601fcefcb04a0e3f2d6c
annnn <AnnnaSukhanova>
2021-12-15 18:47:01 +0400

    add test button

--- src/main/kotlin/ru/smak/ui/MainFrame.kt
+++ src/main/kotlin/ru/smak/ui/MainFrame.kt
@@ -10,12 +10,16 @@ import java.awt.Dimension
 import java.awt.event.KeyEvent
 import java.awt.event.KeyListener
 import javax.swing.GroupLayout
+import javax.swing.JButton
 import javax.swing.JFrame
+import javax.swing.JPanel
 import kotlin.random.Random
 
 class MainFrame : JFrame() {
 
     val fractalPanel: SelectablePanel
+    val controlPanel: JPanel=JPanel()
+    val bSave:JButton= JButton("Save Im")
 
     init{
         defaultCloseOperation = EXIT_ON_CLOSE
@@ -44,13 +48,31 @@ class MainFrame : JFrame() {
             setHorizontalGroup(
                 createSequentialGroup()
                     .addGap(4)
-                    .addComponent(fractalPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+                    .addGroup(
+                        createParallelGroup()
+                            .addComponent(fractalPanel, GroupLayout.DEFAULT_SIZE,  GroupLayout.DEFAULT_SIZE,  GroupLayout.DEFAULT_SIZE)
+                            .addComponent(controlPanel, GroupLayout.DEFAULT_SIZE,  GroupLayout.DEFAULT_SIZE,  GroupLayout.DEFAULT_SIZE)
+                    )
                     .addGap(4)
             )
             setVerticalGroup(
                 createSequentialGroup()
                     .addGap(4)
                     .addComponent(fractalPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+                    .addGap(4)
+                    .addComponent(controlPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+                    .addGap(4)
+            )
+        }
+        controlPanel.layout=GroupLayout(controlPanel).apply{ //делаем контрольную панель
+            setHorizontalGroup(createSequentialGroup()
+                .addGap(8)
+                .addComponent(bSave, GroupLayout.PREFERRED_SIZE,  GroupLayout.PREFERRED_SIZE,  GroupLayout.PREFERRED_SIZE)
+                .addGap(8))
+            setVerticalGroup(
+                createSequentialGroup()
+                    .addGap(4)
+                    .addComponent(bSave, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
                     .addGap(4)
             )
         }
