88905fa4e0e3477d7a5da2a318bda0d97ed1434e
annnn <AnnnaSukhanova>
2021-12-16 22:56:54 +0400

    delete my test btn

--- src/main/kotlin/ru/smak/ui/MainFrame.kt
+++ src/main/kotlin/ru/smak/ui/MainFrame.kt
@@ -7,23 +7,18 @@ import ru.smak.ui.painting.fractals.FractalPainter
 import ru.smak.ui.painting.fractals.colorizers
 import java.awt.Color
 import java.awt.Dimension
-import java.io.File
-import java.io.IOException
-import javax.imageio.ImageIO
-import javax.swing.*
+import javax.swing.GroupLayout
+import javax.swing.JFrame
 import kotlin.random.Random
 
 class MainFrame : JFrame() {
 
     val fractalPanel: SelectablePanel
-    val controlPanel: JPanel=JPanel()
-    val bSave:JButton= JButton("Save Im")
-    val painter: FractalPainter
 
     init{
         defaultCloseOperation = EXIT_ON_CLOSE
         minimumSize = Dimension(600, 400)
-        painter = FractalPainter(
+        val painter = FractalPainter(
             Mandelbrot(),
             CartesianPlane(-2.0, 1.0, -1.0, 1.0),
             colorizers[Random.nextInt(colorizers.size)])
@@ -43,51 +38,17 @@ class MainFrame : JFrame() {
             }
         }
 
-        bSave.addActionListener {
-            val fileChs = JFileChooser()
-            fileChs.dialogTitle = "Choose place to save image"
-            fileChs.fileSelectionMode = JFileChooser.FILES_ONLY
-            val res = fileChs.showSaveDialog(this@MainFrame)
-            if (res == JFileChooser.APPROVE_OPTION) {
-                val im = painter.getImage()
-                val outputFile = File(fileChs.selectedFile.path + ".jpg")
-                    try {
-                        ImageIO.write(im, "jpg", outputFile)
-                    } catch (e: IOException) {
-                        e.printStackTrace()
-                    }
-            }
-        }
-
         layout = GroupLayout(contentPane).apply {
             setHorizontalGroup(
                 createSequentialGroup()
                     .addGap(4)
-                    .addGroup(
-                        createParallelGroup()
-                            .addComponent(fractalPanel, GroupLayout.DEFAULT_SIZE,  GroupLayout.DEFAULT_SIZE,  GroupLayout.DEFAULT_SIZE)
-                            .addComponent(controlPanel, GroupLayout.DEFAULT_SIZE,  GroupLayout.DEFAULT_SIZE,  GroupLayout.DEFAULT_SIZE)
-                    )
+                    .addComponent(fractalPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
                     .addGap(4)
             )
             setVerticalGroup(
                 createSequentialGroup()
                     .addGap(4)
                     .addComponent(fractalPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
-                    .addGap(4)
-                    .addComponent(controlPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
-                    .addGap(4)
-            )
-        }
-        controlPanel.layout=GroupLayout(controlPanel).apply{ //делаем контрольную панель
-            setHorizontalGroup(createSequentialGroup()
-                .addGap(8)
-                .addComponent(bSave, GroupLayout.PREFERRED_SIZE,  GroupLayout.PREFERRED_SIZE,  GroupLayout.PREFERRED_SIZE)
-                .addGap(8))
-            setVerticalGroup(
-                createSequentialGroup()
-                    .addGap(4)
-                    .addComponent(bSave, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
                     .addGap(4)
             )
         }
