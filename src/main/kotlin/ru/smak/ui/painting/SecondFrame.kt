af9034cf83851f3032cfa108d398e5bf33758ce5
Arthur Shaimardanov <artur10708@gmail.com>
2021-12-17 11:52:35 +0300

    второе окно

---
+++ src/main/kotlin/ru/smak/ui/painting/SecondFrame.kt
@@ -0,0 +1,66 @@
+package ru.smak.ui
+
+import org.kotlinmath.Complex
+import ru.smak.math.fractals.Julia
+import ru.smak.math.fractals.Mandelbrot
+import ru.smak.ui.painting.CartesianPlane
+import ru.smak.ui.painting.SelectablePanel
+import ru.smak.ui.painting.fractals.FractalPainter
+import ru.smak.ui.painting.fractals.colorizers
+import java.awt.Color
+import java.awt.Dimension
+import java.awt.SystemColor.window
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+import javax.swing.GroupLayout
+import javax.swing.JFrame
+import kotlin.random.Random
+
+
+class SecondFrame : JFrame() {
+
+    val fractalPanel: SelectablePanel
+
+
+    init{
+        // defaultCloseOperation = EXIT_ON_CLOSE
+        minimumSize = Dimension(600, 400)
+        val painter = FractalPainter(
+            Julia(),
+            CartesianPlane(-1.0, 1.0, -1.0, 1.0),
+            colorizers[Random.nextInt(colorizers.size)])
+
+        fractalPanel = SelectablePanel(painter).apply {
+            background = Color.WHITE
+            addSelectListener{
+                with (painter.plane){
+                    val xMin = xScr2Crt(it.x)
+                    val yMin = yScr2Crt(it.y)
+                    val xMax = xScr2Crt(it.x + it.width)
+                    val yMax = yScr2Crt(it.y + it.height)
+                    xSegment = Pair(xMin, xMax)
+                    ySegment = Pair(yMin, yMax)
+                }
+                repaint()
+            }
+        }
+
+
+        layout = GroupLayout(contentPane).apply {
+            setHorizontalGroup(
+                createSequentialGroup()
+                    .addGap(4)
+                    .addComponent(fractalPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+                    .addGap(4)
+            )
+            setVerticalGroup(
+                createSequentialGroup()
+                    .addGap(4)
+                    .addComponent(fractalPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+                    .addGap(4)
+            )
+        }
+    }
+
+    //fun onSelectArea(r: Rectangle)
+}
