35636dfcbc0b42a5cc15ca3916f03f3b6d2eb62d
annnn <AnnnaSukhanova>
2021-12-19 16:25:13 +0400

    Добавлена полоска с данными о плоскости, исправлен баш с сохранением

--- src/main/kotlin/ru/smak/ui/painting/fractals/FractalPainter.kt
+++ src/main/kotlin/ru/smak/ui/painting/fractals/FractalPainter.kt
@@ -8,7 +8,6 @@ import java.awt.Color
 import java.awt.Dimension
 import java.awt.Graphics
 import java.awt.image.BufferedImage
-import java.io.FileOutputStream
 import java.util.concurrent.Callable
 import java.util.concurrent.Executors
 
@@ -28,23 +27,36 @@ class FractalPainter(
     private val threadCount = 16*4
     private val taskPerThreadCount = 5
     private var fracService = Executors.newFixedThreadPool(threadCount)
-
+    private val rSize = 25
+    private val nSize = 80
     private val list = mutableListOf<BufferedImage>()
 
 
     val image: BufferedImage
     get() {
-        val image = BufferedImage(plane.width, plane.height, BufferedImage.TYPE_INT_RGB)
+        val image = BufferedImage(plane.width, plane.height+rSize, BufferedImage.TYPE_INT_RGB)
         val g1 = image.graphics
         for (i in 0 until  list.size) {
             g1.drawImage(list[i],  list[0].width*i,0, list[i].width,  list[i].height, null)
         }
+        g1.color = Color.BLACK
+        g1.fillRect(0, plane.height, list[0].width*list.size, rSize)
+        g1.color= Color.WHITE
+        g1.drawString( "xMin="+String.format("%.3f", plane.xMin)  , 10 , plane.height+rSize/2)
+        g1.drawString( "xMax="+String.format("%.3f", plane.xMax)  , 10 + nSize , plane.height+rSize/2)
+        g1.drawString( "yMin="+String.format("%.3f", plane.yMin)  , 10 + 2*nSize , plane.height+rSize/2)
+        g1.drawString( "yMax="+String.format("%.3f", plane.yMax)  , 10 + 3*nSize , plane.height+rSize/2)
         return image
     }
 
 
     override fun paint(g: Graphics) {
         with(plane){
+            if (list.size!=0){
+               for (i in 0 until list.size){
+                list.removeAt(i)
+                }
+            }
             if (width <= 0 || height <= 0) return
             if (!(fracService.isShutdown || fracService.isTerminated)) {
                 fracService.shutdown()
